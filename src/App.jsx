2478b78072d9ce697cc06f110831cb762bee2d46
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-08-23 17:39:44 +0530

    btt

--- src/App.jsx
+++ src/App.jsx
@@ -93,7 +93,7 @@ function App() {
                 <Route path="*" element={<PageNotFound />} />
             </Routes>
             <Footer />
-            <BackToUp>Top</BackToUp>
+            <BackToUp size={40}>Top</BackToUp>
         </main>
 
     );
