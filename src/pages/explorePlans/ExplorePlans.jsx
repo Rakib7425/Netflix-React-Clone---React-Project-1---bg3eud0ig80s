9ecf9423caece9cd67c4213beeea034e362502fd
rakibul Islam <irakibul026@gmail.com>
2023-10-27 11:14:20 +0530

    plan subscribe btn style

--- src/pages/explorePlans/ExplorePlans.jsx
+++ src/pages/explorePlans/ExplorePlans.jsx
@@ -1,56 +1,53 @@
-import React from 'react';
-import PlanCard from './PlanCard';
+import React from "react";
+import PlanCard from "./PlanCard";
 
-
-import './styles.scss';
-import ContentWrapper from '../../components/contentWrapper/ContentWrapper';
+import "./styles.scss";
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 
 const plans = [
-    {
-        name: 'Free',
-        price: '₹0.00/month',
-        description: 'Access to basic features and content.'
-    },
-    {
-        name: 'Basic Plan',
-        price: '₹199/month',
-        description: 'Access to basic features and something extra.'
-    },
-    {
-        name: 'Standard Plan',
-        price: '₹399/month',
-        description: 'HD streaming and access to more content.'
-    },
-    {
-        name: 'Premium Plan',
-        price: '₹599/month',
-        description: 'Ultra HD streaming and access to all content two user can use at one time.'
-    },
-    {
-        name: 'Family Plan',
-        price: '₹799/month',
-        description: 'Ultra HD streaming and access to all content six user can use at one time.'
-    },
+	{
+		name: "Free",
+		price: "₹0.00/month",
+		description: "Access to basic features and content for one screen.",
+		active: true,
+	},
+	{
+		name: "Basic Plan",
+		price: "₹199/month",
+		description: "Access to basic features and something extra.",
+	},
+	{
+		name: "Standard Plan",
+		price: "₹399/month",
+		description: "HD streaming and access to more content.",
+	},
+	{
+		name: "Premium Plan",
+		price: "₹599/month",
+		description: "Ultra HD streaming and access to all content two user can use at one time.",
+	},
+	{
+		name: "Family Plan",
+		price: "₹799/month",
+		description: "Ultra HD streaming and access to all content six user can use at one time.",
+	},
 ];
 
 const ExplorePlans = () => {
-    return (
-        <ContentWrapper>
-            <div className="explore-plans">
-                <div className="pageTitle">
-                    Explore Plans.
-                </div>
-                <div className="plans">
-                    <div className="plan-list">
-                        {plans.map((plan, index) => (
-                            <PlanCard key={index} plan={plan} />
-                        ))}
-                    </div>
-                </div>
-            </div>
-        </ContentWrapper>
-    );
+	return (
+		<ContentWrapper>
+			<div className='explore-plans'>
+				<div className='pageTitle'>Explore Plans.</div>
+				<div className='plans'>
+					<div className='plan-list'>
+						{plans.map((plan, index) => (
+							<PlanCard key={index} plan={plan} />
+						))}
+					</div>
+				</div>
+			</div>
+		</ContentWrapper>
+	);
 };
-
 
 export default ExplorePlans;
--- src/pages/explorePlans/PlanCard.jsx
+++ src/pages/explorePlans/PlanCard.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import './planCard.scss';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-
+import React from "react";
+import "./planCard.scss";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const PlanCard = ({ plan }) => {
-    const newtonUser = useSelector((state) => state?.user?.userDetails?.data);
-    const firebaseUser = useSelector((state) => state?.user?.userDetails?.user);
+	const newtonUser = useSelector((state) => state?.user?.userDetails?.data);
+	const firebaseUser = useSelector((state) => state?.user?.userDetails?.user);
 
-    return (
-        <div className="plan-card">
-            <h3 className="plan-name">{plan.name}</h3>
-            <p className="plan-price">{plan.price}</p>
-            <p className="plan-description">{plan.description}</p>
-            <Link to={(newtonUser || firebaseUser) ? '/' : '/login'} className="subscribe-button">Subscribe</Link>
-        </div>
-    );
+	return (
+		<div className='plan-card'>
+			<h3 className='plan-name'>{plan.name}</h3>
+			<p className='plan-price'>{plan.price}</p>
+			<p className='plan-description'>{plan.description}</p>
+			<Link
+				to={newtonUser || firebaseUser ? "/" : "/login"}
+				className={plan.active ? "active " : "subscribe-button"}
+			>
+				Subscribe{plan.active ? "d" : ""}
+			</Link>
+		</div>
+	);
 };
 
 export default PlanCard;
